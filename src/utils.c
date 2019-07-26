b1fb7163a5faf5a5c87eb2a0195f2e0d6532ac7d
thalvari <tuomas.halvari@helsinki.fi>
2019-07-26 21:56:16 +0300

    small changes

--- src/utils.c
+++ src/utils.c
@@ -229,7 +229,7 @@ void find_replace_extension(char *str, char *orig, char *rep, char *output)
 void replace_image_to_label(const char* input_path, char* output_path)
 {
     find_replace(input_path, "/images/train2014/", "/labels/train2014/", output_path);    // COCO
-    find_replace(output_path, "/images/val2014/", "/labels/val2014/", output_path);        // COCO
+    find_replace(output_path, "tmp/val2017/", "data/val2017_labels/", output_path);        // COCO
     find_replace(output_path, "/JPEGImages/", "/labels/", output_path);    // PascalVOC
     find_replace(output_path, "\\images\\train2014\\", "\\labels\\train2014\\", output_path);    // COCO
     find_replace(output_path, "\\images\\val2014\\", "\\labels\\val2014\\", output_path);        // COCO
